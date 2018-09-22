774bfb6c3ea4c7b86bc822091f01c414728eff7f
liuyang <shenshiyishao@163.com>
2018-09-23 05:08:22 +0800

    完成

--- src/main/java/com/baidu/mailtest/service/MailService.java
+++ src/main/java/com/baidu/mailtest/service/MailService.java
@@ -3,9 +3,16 @@ package com.baidu.mailtest.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /******************************
  * @author : liuyang
@@ -36,6 +43,8 @@ public class MailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    public final static Integer TO_MAIL_COUNT = 3;
+
 
     /**
      * 一般发送邮件方法
@@ -54,5 +63,145 @@ public class MailService {
 
         javaMailSender.send(simpleMailMessage);
     }
+
+    /**
+     * 发送一封Html的邮件
+     *
+     * @param to      发送给某人
+     * @param subject 邮件主题
+     * @param context 邮件内容
+     */
+    public void sendHtmlMail(String to, String subject, String context) {
+
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+            mimeMessageHelper.setFrom(fromUserName);
+
+            mimeMessageHelper.setTo(to);
+
+            mimeMessageHelper.setSubject(subject);
+
+            mimeMessageHelper.setText(context, true);
+
+            javaMailSender.send(mimeMessage);
+            log.info("html邮件发送成功");
+        } catch (MessagingException e) {
+            log.error("发送html邮件时发生异常！", e);
+        }
+
+    }
+
+    /**
+     * 发送一封带一个附件的邮件
+     *
+     * @param to       发送给某人
+     * @param subject  邮件主题
+     * @param content  邮件内容
+     * @param filePath 邮件附件的文件地址
+     */
+    public void sendAttachmentsMail1(String to, String subject, String content, String filePath) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+
+            mimeMessageHelper.setFrom(fromUserName);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(content, true);
+
+            FileSystemResource file = new FileSystemResource(new File(filePath));
+            String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
+            mimeMessageHelper.addAttachment(fileName, file);
+
+            javaMailSender.send(mimeMessage);
+            log.info("带附件的邮件已经发送。");
+        } catch (MessagingException e) {
+            log.error("发送带附件的邮件时发生异常！", e);
+        } catch (StringIndexOutOfBoundsException e) {
+            log.error("路径名字长度越界！", e);
+        }
+    }
+
+    /**
+     * 发送一封带多个附件的邮件
+     *
+     * @param to       发送给某人
+     * @param subject  邮件主题
+     * @param content  邮件内容
+     * @param filePath 邮件附件的文件地址
+     */
+    public void sendAttachmentsMail2(String to, String subject, String content, String filePath) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+
+            mimeMessageHelper.setFrom(fromUserName);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(content, true);
+
+            FileSystemResource file = new FileSystemResource(new File(filePath));
+
+            //发送多个附件,将fileName做成数组,然后遍历给addAttachment进行多个不重复的附件发送.
+            String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
+
+            //添加多个邮件的附件
+            for (int i = 0; i < TO_MAIL_COUNT; i++) {
+                mimeMessageHelper.addAttachment(fileName + i, file);
+            }
+
+            javaMailSender.send(mimeMessage);
+            log.info("带附件的邮件已经发送。");
+        } catch (MessagingException e) {
+            log.error("发送带附件的邮件时发生异常！", e);
+        } catch (StringIndexOutOfBoundsException e) {
+            log.error("路径名字长度越界！", e);
+        } catch (MailSendException e) {
+            log.error("发送路径异常！", e);
+        }
+    }
+
+
+    /**
+     * 发送静态资源,即图片发送的方法
+     *
+     * @param to           发送给某人
+     * @param subject      邮件主题
+     * @param content      邮件内容
+     * @param resourcePath 资源路径
+     * @param resourceId   图片编号
+     */
+    public void sendInlineResourceMail(String to, String subject, String content, String resourcePath, String resourceId) {
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(fromUserName);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(content, true);
+
+            FileSystemResource fileSystemResource = new FileSystemResource(new File(resourcePath));
+            mimeMessageHelper.addInline(resourceId, fileSystemResource);
+
+            javaMailSender.send(mimeMessage);
+            log.info("嵌入静态资源的邮件已经发送。");
+        } catch (MessagingException e) {
+            log.error("发送嵌入静态资源的邮件时发生异常！", e);
+        } catch (MailSendException e) {
+            log.error("文件路径过长异常！", e);
+        }
+    }
+
 
 }
--- src/test/java/com/baidu/mailtest/MailServiceApplicationTests.java
+++ src/test/java/com/baidu/mailtest/MailServiceApplicationTests.java
@@ -4,10 +4,14 @@ import com.baidu.mailtest.service.MailService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,14 +19,88 @@ import javax.annotation.Resource;
 public class MailServiceApplicationTests {
 
     @Resource
-    MailService mailService;
+    private MailService mailService;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    public final static String TO_USER_NAME = "liuyang@easylabplus.com";
+
+    /**
+     * 一般测试
+     */
     @Test
-    public void sayMail() {
+    public void sendSimpleMail() {
 
-        mailService.sendSimpleMail("liuyang@easylabplus.com", "SpringBoot邮件测试", "今天需要很多美女陪我");
+        mailService.sendSimpleMail(TO_USER_NAME, "SpringBoot邮件测试", "今天需要很多美女陪我");
 
-        log.info("发送成功");
+        log.info("sendSimpleMail" + ":" + "发送成功");
     }
 
+    /**
+     * html测试
+     */
+    @Test
+    public void sendHtmlMail() {
+
+
+        String htmlString = "<html>\n" +
+                "<body>\n" +
+                "    <h3>hello world ! 这是一封Html邮件!</h3>\n" +
+                "</body>\n" +
+                "</html>";
+
+        mailService.sendHtmlMail(TO_USER_NAME, "SpringBoot邮件测试", htmlString);
+
+        log.info("sendHtmlMail" + ":" + "发送成功");
+    }
+
+
+    /**
+     * 附件测试1
+     */
+    @Test
+    public void sendAttachmentsMail1() {
+        String filePath = "C:\\Users\\Administrator\\Desktop\\SpringCloud2018.mmap";
+        mailService.sendAttachmentsMail1(TO_USER_NAME, "主题：带附件的邮件", "有附件，请查收！", filePath);
+    }
+
+    /**
+     * 附件测试2
+     */
+    @Test
+    public void sendAttachmentsMail2() {
+
+        String filePath = "C:\\Users\\Administrator\\Desktop\\rentest.sql";
+
+        mailService.sendAttachmentsMail2(TO_USER_NAME, "主题：带附件的邮件", "有附件，请查收！", filePath);
+    }
+
+    /**
+     * 图片测试
+     */
+    @Test
+    public void sendInlineResourceMail() {
+        String imagePath = "C:\\Users\\Administrator\\Pictures\\1.png";
+        String resourceId = "1";
+        String content = "<html><body>这是有图片的邮件:\n<img src=\'cid:" + resourceId + "\'></body></html>";
+
+        mailService.sendInlineResourceMail("shenshiyishao@163.com", "主题：PNG邮件", content, imagePath, resourceId);
+    }
+
+
+
+    @Test
+    public void sendTemplateMail() {
+        //创建邮件正文
+        Context context = new Context();
+        context.setVariable("id", "006");
+        String emailContent = templateEngine.process("emailTemplate", context);
+
+//        mailService.sendHtmlMail("shenshiyishao@163.com","主题：这是模板邮件",emailContent);
+        String imagePath = "C:\\Users\\Administrator\\Pictures\\1.png";
+        String resourceId = "1";
+        mailService.sendInlineResourceMail("shenshiyishao@163.com", "主题：PNG邮件", emailContent, imagePath, resourceId );
+
+    }
 }
